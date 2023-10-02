6909e05fdce599127c134ff67c8446a6d47741db
Vincent Baraga <vincentb.baraga@gmail.com>
2023-10-02 18:13:29 -0400

    add the five day forecast functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,3 @@
-// a function "event listener" for when the button is clicked, capture the input in the text field
-// a function for if the text input is not null, add the variable containing the input to the api string in the city name position
-// this function also fetches the api with the new concated text input if it's not null, it then runs a function dedicated to displaying what it's found
-//run this function in the event listener
-// create an empty array which is filled with searchCity's from localstorage on page load. if array != null {loop the creation of buttons for each index in the array}
-//create a title div with the data.name and date (added in the second fetch), then a <p> div with the "temp:" and main.temp object key
-// another <p> "humidity:" + data.main.humidity then a  <p> with "wind:" + data.wind.speed
-//five day forcast data is under the second fetch data.list[2-6].values
-// let url =
-//   "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=e6e752195833b1e15bb3055c75163dfd";
-// let testURL =
-//   "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e6e752195833b1e15bb3055c75163dfd";
-let testURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e6e752195833b1e15bb3055c75163dfd";
 let searchBtn = document.querySelector("#searchBtn");
 let searchElement = document.querySelector("#searchElement");
 let historyElement = document.querySelector("#searchHistory");
@@ -20,8 +6,11 @@ let citynameEl = document.querySelector("#citynameEl");
 let citytempEl = document.querySelector("#citytempEl");
 let citywindEl = document.querySelector("#citywindEl");
 let cityhumEl = document.querySelector("#cityhumEl");
-console.log(searchBtn);
-console.log(searchElement);
+let fivedaySection = document.querySelector("#fivedaySection");
+let fivedayTitle = document.querySelector("#fivedayTitle");
+
+// console.log(searchBtn);
+// console.log(searchElement);
 
 let cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
 
@@ -33,8 +22,10 @@ if (cityHistory !== null) {
 }
 
 function displayHistory() {
+  historyElement.innerHTML = "";
   for (let i = 0; i < cityHistory.length; i++) {
     let historyBtn = document.createElement("button");
+    historyBtn.classList.add("m-2");
     historyBtn.textContent = cityHistory[i];
     historyElement.appendChild(historyBtn);
     historyBtn.addEventListener("click", function () {
@@ -45,7 +36,10 @@ function displayHistory() {
 }
 
 function getcityInfo() {
+  fivedaySection.innerHTML = "";
   let searchedCity = searchElement.value.trim();
+  fivedaySection.classList.remove("is-hidden");
+  fivedayTitle.classList.remove("is-hidden");
   currentWeather.classList.remove("is-hidden");
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=e6e752195833b1e15bb3055c75163dfd&units=imperial`;
 
@@ -53,7 +47,7 @@ function getcityInfo() {
   if (searchedCity != "") {
     cityHistory.push(searchedCity);
 
-    console.log(cityHistory);
+    // console.log(cityHistory);
 
     localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
 
@@ -66,7 +60,7 @@ function getcityInfo() {
         }
       })
       .then(function (data) {
-        console.log(data);
+        // console.log(data);
 
         let cityName = data.name;
 
@@ -74,7 +68,7 @@ function getcityInfo() {
 
         let cityTemp = data.main.temp;
 
-        citytempEl.textContent = `Temp: ${cityTemp} Degrees`;
+        citytempEl.textContent = `Temp: ${cityTemp}°F`;
 
         let cityHumidity = data.main.humidity;
 
@@ -89,14 +83,14 @@ function getcityInfo() {
         // console.log(citytempEl.textContent);
         let cityLat = data.coord.lat;
 
-        console.log(cityLat);
+        // console.log(cityLat);
 
         let cityLong = data.coord.lon;
 
-        console.log(cityLong);
+        // console.log(cityLong);
 
         let forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLong}&appid=e6e752195833b1e15bb3055c75163dfd&units=imperial`;
-        console.log(forecastUrl);
+        // console.log(forecastUrl);
 
         return fetch(forecastUrl);
       })
@@ -109,9 +103,62 @@ function getcityInfo() {
       })
       .then(function (data) {
         console.log(data);
+        for (let i = 0; i < data.list.length; i++) {
+          let hourCheck = data.list[i].dt_txt;
+
+          if (hourCheck.includes("00:00:00")) {
+            let dayDate = hourCheck.split(" ")[0];
+            let dayTemp = data.list[i].main.temp;
+            let dayWind = data.list[i].wind.speed;
+            let dayHumidity = data.list[i].main.humidity;
+            // console.log(dayDate);
+            // console.log(dayTemp);
+            // console.log(dayWind);
+            // console.log(dayHumidity);
+            let dayCard = document.createElement("div");
+
+            dayCard.className =
+              "card has-background-link-dark has-text-white m-2";
+
+            let daycardDate = document.createElement("h1");
+
+            daycardDate.textContent = dayDate;
+
+            let daycardContent = document.createElement("div");
+
+            daycardContent.className = "card-content";
+
+            let daytempEl = document.createElement("p");
+
+            daytempEl.textContent = `Temp: ${dayTemp}°F`;
+
+            daycardContent.appendChild(daytempEl);
+
+            let daywindEl = document.createElement("p");
+
+            daywindEl.textContent = `Wind: ${dayWind}MPH`;
+
+            daycardContent.appendChild(daywindEl);
+
+            let dayhumidityEl = document.createElement("p");
+
+            dayhumidityEl.textContent = `Humidity: ${dayHumidity}%`;
+
+            daycardContent.appendChild(dayhumidityEl);
+
+            daycardDate.appendChild(daycardContent);
+
+            dayCard.appendChild(daycardDate);
+
+            fivedaySection.appendChild(dayCard);
+          }
+        }
       });
   } else {
     console.log("error");
   }
 }
-searchBtn.addEventListener("click", getcityInfo);
+searchBtn.addEventListener("click", function () {
+  getcityInfo();
+  displayHistory();
+});
